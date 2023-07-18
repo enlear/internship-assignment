604f24d9461fec0b0889c20b2fa4e5baf02939c2
Manilka Dilshan <phrmanilkadilshan@gmail.com>
2023-07-18 20:56:50 +0530

    Manilka Dilshan

--- index.js
+++ index.js
@@ -4,9 +4,32 @@ const clearButton = document.getElementById("clear");
 const list = document.getElementById("un-ordered-list");
 const input = document.getElementById("input");
 
-addButton.addEventListener("click", () => {});
-removeButton.addEventListener("click", () => {});
+addButton.addEventListener("click", () => {
+  const userInput = input.value.trim();
+
+  if (userInput !== "") {
+    const newItem = document.createElement("li");
+    newItem.textContent = userInput;
+    list.appendChild(newItem);
+    input.value = "";
+  }
+});
+
+
+removeButton.addEventListener("click", () => {
+  const listItems = document.querySelectorAll("li");
+
+  if (listItems.length > 0) {
+    const lastItemIndex = listItems.length - 1;
+    const lastItem = listItems[lastItemIndex];
+
+    lastItem.remove();
+  }
+});
+
 
 clearButton.addEventListener("click", () => {
-  list.innerHTML = "";
+  setTimeout(() => {
+    list.innerHTML = "";
+  }, 2000);
 });
