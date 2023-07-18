6e74eef8af503bba7bd7432fb69383fe2cc72360
AnjanaPasinduMadushan <pbapmadushan@gmail.com>
2023-07-19 00:37:41 +0530

    Feature 03 completed - remove button works

--- index.js
+++ index.js
@@ -5,20 +5,29 @@ const list = document.getElementById("un-ordered-list");
 const input = document.getElementById("input");
 
 addButton.addEventListener("click", () => {
-
+  
   const newlistitem = document.createElement("li");
   newlistitem.innerText = input.value;
   list.appendChild(newlistitem)
+  input.value = "";
 });
   
 removeButton.addEventListener("click", () => {
-  
+  if(list.hasChildNodes()){
+    list.removeChild(list.lastElementChild);
+  }else{
+    alert('There is nothing to remove in the List');
+  }
 });
 
 clearButton.addEventListener("click", () => {
   
+  if(list.hasChildNodes()){
   setTimeout(()=>{
     list.innerHTML = "";
     }, 2000)
+  }else{
+    alert('There is nothing to clear in the List');
+  }
     
 });
