615e49884465aa68e1f570c45ee31350eae5e17e
ruwaidhahakeem <h.ruwaidha@gmail.com>
2023-07-19 18:26:14 +0530

    Ruwaidha-Hakeem-Pull-Req

--- index.js
+++ index.js
@@ -4,9 +4,36 @@ const clearButton = document.getElementById("clear");
 const list = document.getElementById("un-ordered-list");
 const input = document.getElementById("input");
 
-addButton.addEventListener("click", () => {});
-removeButton.addEventListener("click", () => {});
+
+addButton.addEventListener("click", addListItem);
+removeButton.addEventListener("click", removeListItem);
+
 
 clearButton.addEventListener("click", () => {
-  list.innerHTML = "";
-});
+  setTimeout(() => {
+    list.innerHTML = "";
+  }, 2000);
+
+}
+);
+
+
+function addListItem() {
+  let newItem = document.createElement('li');
+
+  if (input.value !== "") {
+    newItem.textContent = input.value;
+    list.appendChild(newItem);
+    input.value = "";
+  }
+  else {
+    alert("Please input an item")
+  }
+}
+
+function removeListItem() {
+  //last child of list
+
+  let lastItem = list.lastElementChild;
+  list.removeChild(lastItem);
+}
