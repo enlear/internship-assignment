746b3368616e90b7c091ac263d8c8458370801c3
Chamoda <it21268076@my.sliit.lk>
2023-07-19 12:13:29 +0530

    Bug 01 & Bug 02 are fixed

--- index.js
+++ index.js
@@ -4,9 +4,24 @@ const clearButton = document.getElementById("clear");
 const list = document.getElementById("un-ordered-list");
 const input = document.getElementById("input");
 
-addButton.addEventListener("click", () => {});
-removeButton.addEventListener("click", () => {});
+addButton.addEventListener("click", () => {
+
+  if(input.value != ""){
+    let create_list = document.createElement("li");
+    create_list.appendChild(document.createTextNode(input.value));
+    list.appendChild(create_list);
+
+    input.value = ""
+  }
+  else{
+    alert("Please enter your TO-DO item!!");
+  }
+});
+
+removeButton.addEventListener("click", () => {
+});
 
 clearButton.addEventListener("click", () => {
   list.innerHTML = "";
 });
+
