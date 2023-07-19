10f8cf43a2a9d2faddeb73cace23e695905c60f3
nishanthan <nishanthan@codevus.com>
2023-07-19 10:05:07 +0530

    task done

--- index.js
+++ index.js
@@ -4,9 +4,39 @@ const clearButton = document.getElementById("clear");
 const list = document.getElementById("un-ordered-list");
 const input = document.getElementById("input");
 
-addButton.addEventListener("click", () => {});
-removeButton.addEventListener("click", () => {});
+addButton.addEventListener("click", () => {
+  //get the user input and remove whitespace
+  const userInput = input.value.trim();
+
+  if (userInput !== "") {
+    const listItem = document.createElement("li");
+    listItem.textContent = userInput;
+
+    list.appendChild(listItem);
+
+    //clear the field after the input completed
+    input.value = "";
+  } else {
+    alert("The input field is empty!");
+  }
+});
+
+removeButton.addEventListener("click", () => {
+  if (list.lastChild) {
+    list.removeChild(list.lastChild);
+  } else {
+    //show alert when list is empty
+    alert("The list is empty!");
+  }
+});
 
 clearButton.addEventListener("click", () => {
-  list.innerHTML = "";
+  if (list.hasChildNodes()) {
+    setTimeout(() => {
+      list.innerHTML = "";
+    }, 2000);
+  } else {
+    //show alert when list is empty
+    alert("Nothing to clear!");
+  }
 });
