258aa267661dba18cc6be771362f4e56bca3b6f4
yohan9878 <it20256050@my.sliit.lk>
2023-07-19 11:36:03 +0530

    assesment completed

--- index.js
+++ index.js
@@ -4,8 +4,23 @@ const clearButton = document.getElementById("clear");
 const list = document.getElementById("un-ordered-list");
 const input = document.getElementById("input");
 
-addButton.addEventListener("click", () => {});
-removeButton.addEventListener("click", () => {});
+addButton.addEventListener("click", () => {
+  const text = input.value.trim();
+  if (text !== "") {
+    const listItem = document.createElement("li");
+    listItem.textContent = text;
+    list.appendChild(listItem);
+    input.value = "";
+  }
+});
+
+removeButton.addEventListener("click", () => {
+  const listItems = list.getElementsByTagName("li");
+  const lastItemIndex = listItems.length - 1;
+  if (lastItemIndex >= 0) {
+    list.removeChild(listItems[lastItemIndex]);
+  }
+});
 
 clearButton.addEventListener("click", () => {
   list.innerHTML = "";
